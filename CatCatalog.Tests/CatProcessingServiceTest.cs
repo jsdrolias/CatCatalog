d82dae350f5bfdfb5ae1e3abe1e806ea5fd145c8
jsdrolias <jsdrolias@gmail.com>
2025-04-02 13:09:19 +0300

    add unit tests

--- CatCatalog.Tests/CatProcessingServiceTest.cs
+++ CatCatalog.Tests/CatProcessingServiceTest.cs
@@ -13,38 +13,22 @@ namespace CatCatalog.Tests;
 
 public class CatProcessingServiceTest
 {
-    private readonly CatProcessingService _sut;
-    private readonly IBlobStorageService _blobStorageService;
-    private readonly ICatWebClientService _catWebClientService;
-    private readonly ILogger<CatProcessingService> _logger;
-    private readonly Mock<CatDbContext> _catDbContext;
+    private IBlobStorageService _blobStorageService;
+    private ICatWebClientService _catWebClientService;
+    private ILogger<CatProcessingService> _logger;
+    private Mock<CatDbContext> _catDbContext;
     private Mock<DbSet<Cat>> _dbCat;
     private Mock<DbSet<Tag>> _dbTag;
+    private List<Tag> _tagList = new();
 
     public CatProcessingServiceTest()
     {
         var options = new DbContextOptionsBuilder<CatDbContext>()
              .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
              .Options;
-
         _catWebClientService = Substitute.For<ICatWebClientService>();
-
-        _dbTag = CreateMockDbSet(new List<Tag>());
-        _dbCat = CreateMockDbSet(new List<Cat>());
-
-        var mockContext = new Mock<CatDbContext>();
-        mockContext.Setup(c => c.Tag).Returns(_dbTag.Object);
-        mockContext.Setup(c => c.Cat).Returns(_dbCat.Object);
-        _catDbContext = mockContext;
-
         _blobStorageService = Substitute.For<IBlobStorageService>();
         _logger = Substitute.For<ILogger<CatProcessingService>>();
-
-        _sut = new CatProcessingService(
-            _catWebClientService,
-            mockContext.Object,
-            _blobStorageService,
-            _logger);
     }
 
     [Theory]
@@ -71,7 +55,7 @@ public class CatProcessingServiceTest
             .Returns(url2);
 
         // Act
-        await _sut.FetchImages();
+        await _sut(new List<Tag>()).FetchImages();
 
         // Assert
         var expectedCat1 = _catDbContext.Object.Cat.First();
@@ -87,6 +71,69 @@ public class CatProcessingServiceTest
         expectedCat2.Width.Should().Be(cat2.Width);
         expectedCat2.Image.Should().Be(url2);
         expectedCat2.Tags.Should().BeEmpty();
+    }
+
+    [Theory]
+    [AutoNSubstituteData]
+    public async Task FetchImages_WithTags_StoresCatWithLowercaseTags(
+      CatFromWebDTO cat,
+      string url)
+    {
+        // Arrange
+        cat.Breeds.Clear();
+        var tag1 = "tag1";
+        var tag2 = "TAG2";
+        var tag3 = "tAg";
+        cat.Breeds.Add(new BreedFromWebDTO { Temperament = $"{tag1}, {tag2}, {tag3}" });
+        List<CatFromWebDTO> cats = new()
+        {
+            cat
+        };
+        _catWebClientService.FetchCatImagesAsync(Arg.Any<int>())
+            .Returns(cats);
+        _blobStorageService.DownloadAndUploadImageAsync(cat.ImageUrl, cat.ImageId + ".jpg")
+            .Returns(url);
+
+        _tagList = new List<Tag>() 
+        { 
+            new Tag { Name = tag1.ToLowerInvariant() } ,
+            new Tag { Name = tag2.ToLowerInvariant() } ,
+            new Tag { Name = tag3.ToLowerInvariant() }
+        };
+
+        // Act
+        await _sut(_tagList).FetchImages();
+
+        // Assert
+        var expectedCat1 = _catDbContext.Object.Cat.First();
+        expectedCat1.CatId.Should().Be(cat.ImageId);
+        expectedCat1.Height.Should().Be(cat.Height);
+        expectedCat1.Width.Should().Be(cat.Width);
+        expectedCat1.Image.Should().Be(url);
+        expectedCat1.Tags.Select(b=>b.Name).Should().BeEquivalentTo(new List<string> { tag1.ToLowerInvariant() , tag2.ToLowerInvariant(), tag3.ToLowerInvariant() });
+
+        _catDbContext.Object.Tag.First().Name.Should().Be(tag1.ToLowerInvariant());
+        _catDbContext.Object.Tag.Skip(1).First().Name.Should().Be(tag2.ToLowerInvariant());
+        _catDbContext.Object.Tag.Skip(2).First().Name.Should().Be(tag3.ToLowerInvariant());
+    }
+
+    private CatProcessingService _sut(List<Tag> tagList)
+    {
+        _dbTag = CreateMockDbSet(_tagList);
+        _dbCat = CreateMockDbSet(new List<Cat>());
+
+        var mockContext = new Mock<CatDbContext>();
+        mockContext.Setup(c => c.Tag).Returns(_dbTag.Object);
+        mockContext.Setup(c => c.Cat).Returns(_dbCat.Object);
+        _catDbContext = mockContext;
+
+        var sut = new CatProcessingService(
+            _catWebClientService,
+            mockContext.Object,
+            _blobStorageService,
+            _logger);
+
+        return sut;
     }
 
     public static Mock<DbSet<T>> CreateMockDbSet<T>(List<T> data) where T : class
